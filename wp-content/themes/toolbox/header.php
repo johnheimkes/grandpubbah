493376e137febbc64b361d8720873d6513a1dbd6
cortney <cortney@opticpop.com>
2013-04-28 19:49:07 -0500

    Changing layout around, experimenting
    moved the nav to top of page, tried getting typekit to connent to
    localhost but didn't work, getting the feel for where things are and
    what the layout needs to be

--- wp-content/themes/toolbox/header.php
+++ wp-content/themes/toolbox/header.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
  * The Header for our theme.
  *
@@ -56,6 +56,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<nav id="access" role="navigation">
+			<h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'toolbox' ); ?></h1>
+			<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'toolbox' ); ?>"><?php _e( 'Skip to content', 'toolbox' ); ?></a></div>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #access -->
 <div id="page" class="hfeed">
 <?php do_action( 'before' ); ?>
 	<header id="branding" role="banner">
@@ -63,13 +69,6 @@
 			<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
-
-		<nav id="access" role="navigation">
-			<h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'toolbox' ); ?></h1>
-			<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'toolbox' ); ?>"><?php _e( 'Skip to content', 'toolbox' ); ?></a></div>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #access -->
 	</header><!-- #branding -->
 
 	<div id="main">
