05ca6f15a86e172fdd7c7840b800282ecf9e7a55
cortney <cortney@opticpop.com>
2013-05-09 16:26:38 -0500

    added classroom policy page, footer and other posts

--- wp-content/themes/toolbox/footer.php
+++ wp-content/themes/toolbox/footer.php
@@ -12,6 +12,34 @@
 	</div><!-- #main -->
 
 	<footer id="colophon" role="contentinfo">
+		
+		<!-- FIRST COLUMN, SCHEDULE FOR EACH QUARTER INFO -->
+		<div class="col-footer">
+			<h3 class="footer-h3">Spring 2013 class schedule</h3>
+			<h4 class="footer-h4">Douglas Brull</h4>
+			<p class="schedule-bold">Portfolio / Portfolio Prep</p>
+			<p class="schedule">T/R 5:00pm – 8:00pm</p>
+			<p class="schedule-bold">Design Production Team</p>
+			<p class="schedule">T/R 8:00pm – 11:00pm</p>
+			<h4 class="footer-h4">Matthew Luken</h4>
+			<p class="schedule-bold">Not teaching this quarter</p>
+		</div>
+		
+		<!-- SECOND COLUMN, SCHEDULE FOR EACH QUARTER INFO -->
+		<div class="col-footer">
+			<h3 class="footer-h3">Contact Information &amp; Office Hours</h3>
+			<h4 class="footer-h4">Douglas Brull</h4>
+			<p class="schedule-bold">grandpubbah@gmail.com</p>
+			<p class="schedule-toppadding">If you don't care when I see the email:</p>
+			<p class="schedule-bold">dbrull@aii.edu</p>
+			<h4 class="footer-h4">Matthew Luken</h4>
+			<p class="schedule-bold">ridledidle@gmail.com</p>
+		</div>
+		
+		<!-- THIRD COLUMN, SCHEDULE FOR EACH QUARTER INFO -->
+		<div class="col-footer">
+			<h3 class="footer-h3">I won't be here...</h3>
+		</div>
 		<div class="footer-info">
 			<p>© Grandpubbah. Website built by OpticPop</p>
 		</div>
